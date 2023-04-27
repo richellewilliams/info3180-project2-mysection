cf8aae6fe326f765b7561dc5293ceed16f631389
Richelle Williams <richellewilliams3@gmail.com>
2023-04-27 09:30:42 -0500

    Added additional style to NewPostForm.vue

--- src/components/NewPostForm.vue
+++ src/components/NewPostForm.vue
@@ -8,15 +8,15 @@
                     <div class="form-group">
                         <div class=" pb-4">
                             <label for="photo" class="form-label">Photo</label>
-                            <input type="file" name="photo" class="form-control"/>
+                            <input type="file" name="photo" id="photo"/>
                         </div>
 
                         <div class="form-group pb-4">
                             <label for="caption" class="form-label">Caption</label>
-                            <textarea type="text" name="caption" class="form-control"></textarea>
+                            <textarea type="text" name="caption" class="form-control" placeholder="Write a caption..."></textarea>
                         </div>
         
-                        <button class="btn" type="submit">Register</button>
+                        <button class="btn" type="submit">Submit</button>
                     </div>
                 </form>
             </div>
@@ -67,30 +67,60 @@ function savePost() {
 </script>
 
 <style>
-    .body {
-        padding: 80px 500px 0px 500px;
-    }
+.body {
+    padding: 80px 500px 0px 500px;
+}
     
-    .form-container {
-        border-color: #d8d5cd;
-        border-style: solid;
-        border-radius: 5px;
-        padding: 43px 40px 25px 40px;
-    }
+.form-container {
+    border-color: #d8d5cd;
+    border-style: solid;
+    border-radius: 5px;
+    padding: 43px 40px 25px 40px;
+}
 
-    textarea { 
-        height: 100px;
-        resize: none;
-    } 
+textarea { 
+    height: 100px;
+    resize: none;
+} 
 
-    .btn {
-        background-color: #7ed321;
-        color: white;
-        padding: 5px 136px;
-    }
+.btn {
+    background-color: #7ed321;
+    color: white;
+    padding: 5px 136px;
+}
 
-    .btn:hover {
-        background-color: #7ed321;
-        color: white;
-    }
+.btn:hover {
+    background-color: #7ed321;
+    color: white;
+}
+
+label #photo input[type="file"] {
+position: absolute;
+content: 'Browse';
+}
+  
+#photo {
+    cursor: pointer;
+    padding-right: 0px;
+}
+
+#photo::-webkit-file-upload-button { 
+    visibility: hidden;
+    text-indent: -999em;
+    padding-right: 10px;    
+}
+
+#photo::before {
+    content: 'Browse';
+    cursor: pointer;
+    display: inline-block;
+    border: 1px solid rgb(222,226,230,255);
+    border-radius: 5px;
+    font-size: 15px;
+    outline: none;
+    padding: 2px 20px;
+    -webkit-user-select: none;
+    user-select: none;
+    white-space: nowrap;
+}
 </style>
